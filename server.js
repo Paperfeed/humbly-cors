08be52ec1028037d03b6b8ba91d40aec60236a26
Glitch (hello-express) <none>
2020-08-17 21:59:19 +0000

    💄🍘 Checkpoint ./server.js:1689053/812 ./README.md:1689053/1027 ./views/index.html:1689053/1 ./package.json:1689053/198

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const app = express()
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/ba-sic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html')
-});
-
 app.get('/:url', function(request, response) {
   try {
+  
     get({ url: request.params.url, header: request.headers }, function(err, res, body) {
-      let merged = {...res.headers, ...{
+      const headers = res ? res.headers : []
+      let merged = {...headers, ...{
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'DELETE,GET,PATCH,POST,PUT',
           'Access-Control-Allow-Headers': 'Content-Type,Authorization'
@@ -29,6 +26,7 @@ app.get('/:url', function(request, response) {
 
 app.post('/:url', function(request, response) {
   try {
+    if (request && request.headers) {
     get({ url: request.params.url, header: request.headers }, function(err, res, body) {
       let merged = {...res.headers, ...{
           'Access-Control-Allow-Origin': '*',
@@ -37,17 +35,15 @@ app.post('/:url', function(request, response) {
       }};
       response.set(merged)
       response.send(body)
+      console.log('Sent body', body)
     });
+    } else {
+      response.send('No request header')
+    }
   } catch (err) {
     response.send('500: Guru Meditation Error')
     console.log(err)
   }
-});
-
-// CORS header `Access-Control-Allow-Origin` set to accept all
-app.get('/allow-cors', function(request, response) {
-  response.set('Access-Control-Allow-Origin', '*');
-  response.sendFile(__dirname + '/message.json');
 });
 
 // listen for requests :)
