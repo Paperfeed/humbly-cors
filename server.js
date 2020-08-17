3227c002fd9b274badbe34d95b5c7111d20ce807
Glitch (hello-express) <none>
2020-08-17 21:47:56 +0000

    🛢📈 Checkpoint ./server.js:1689053/5632 ./package.json:1689053/24

--- server.js
+++ server.js
@@ -1,37 +1,53 @@
-// server.js
-// where your node app starts
-
-// init project
-const express = require('express');
-const bent = require('bent');
-const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const express = require('express')
+const get = require('request')
+const app = express()
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/ba-sic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/views/index.html')
 });
 
 app.get('/:url', function(request, response) {
   try {
-    const get = bent('GET')
-  get({ url: request.params.url, header: request.headers }, function(err, res, body) {
-    let merged = {...res.headers, ...{
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'DELETE,GET,PATCH,POST,PUT',
-        'Access-Control-Allow-Headers': 'Content-Type,Authorization'
-    }};
-    response.set(merged);
-    response.send(body);    
-  });
+    get({ url: request.params.url, header: request.headers }, function(err, res, body) {
+      let merged = {...res.headers, ...{
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'DELETE,GET,PATCH,POST,PUT',
+          'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+      }};
+      response.set(merged)
+      response.send(body)
+    });
   } catch (err) {
-    response.send('500: Guru Meditation Error'); 
+    response.send('500: Guru Meditation Error')
+    console.log(err)
   }
+});
+
+app.post('/:url', function(request, response) {
+  try {
+    get({ url: request.params.url, header: request.headers }, function(err, res, body) {
+      let merged = {...res.headers, ...{
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'DELETE,GET,PATCH,POST,PUT',
+          'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+      }};
+      response.set(merged)
+      response.send(body)
+    });
+  } catch (err) {
+    response.send('500: Guru Meditation Error')
+    console.log(err)
+  }
+});
+
+// CORS header `Access-Control-Allow-Origin` set to accept all
+app.get('/allow-cors', function(request, response) {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.sendFile(__dirname + '/message.json');
 });
 
 // listen for requests :)
